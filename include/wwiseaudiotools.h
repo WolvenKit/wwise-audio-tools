d5a8cadb24518783f9a1609a7ef4f6d649dc96f9
Abheek Dhawan <abheekd@protonmail.com>
2022-03-16 10:34:29 -0500

    Create wwiseaudiotools to wrap around existing ww2ogg and revorb functions

---
+++ include/wwiseaudiotools.h
@@ -0,0 +1,3 @@
+#include <string>
+
+std::string wem_to_ogg(std::string indata);
---
+++ src/wwiseaudiotools.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <sstream>
+
+#include "ww2ogg.h"
+#include "revorb.h"
+
+std::string wem_to_ogg(std::string indata) {
+    std::stringstream indata_ss(indata);
+    std::stringstream wem_out;
+    std::stringstream revorb_out;
+
+    if (!ww2ogg(indata_ss.str(), wem_out))
+        return "";
+
+    if (!revorb(wem_out, revorb_out)) {
+        return "";
+    }
+
+    return revorb_out.str();
+}
