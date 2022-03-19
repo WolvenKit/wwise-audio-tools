1ee4223bcae2f4b102038486038114a1efbb8b8b
Abheek Dhawan <abheekd@protonmail.com>
2022-03-18 21:39:59 -0500

    Convert all WEM files in current repository if no file is specified

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -8,24 +9,43 @@
 #include "wwiseaudiotools.h"
 #include "wwriff.h"
 
+namespace fs = std::filesystem;
+
+bool convert(std::string path) {
+		std::ofstream fout(path.substr(0, path.find_last_of(".")) + ".ogg");
+		std::ifstream filein(path);
+
+		std::stringstream indata;
+		indata << filein.rdbuf();
+
+		std::string outdata = wem_to_ogg(indata.str());
+		if (outdata == "") {
+			return false;
+		}
+
+		fout << outdata;
+		return true;
+}
+
 int main(int argc, char *argv[]) {
   if (argc < 2) {
-    std::cerr << "Missing file!" << std::endl;
-    return 1;
-  }
-
-  auto infile = std::string(argv[1]);
-  std::ofstream fout(infile.substr(0, infile.find_last_of(".")) + ".ogg");
-  std::ifstream filein(infile);
-
-  std::stringstream indata;
-  indata << filein.rdbuf();
-
-  std::string outdata = wem_to_ogg(indata.str());
-  if (outdata == "") {
-    std::cerr << "An error occurred." << std::endl;
-    return 1;
-  }
-
-  fout << outdata;
+		// If there is no input file, convert all WEM files in the current directory
+		for (const auto& file : fs::directory_iterator(fs::current_path())) {
+			if (file.path().extension() == ".wem") {
+				std::cout << "Coverting " << file.path() << "..." << std::endl;
+				auto success = convert(file.path());
+				if (!success) {
+					std::cerr << "Failed to convert " << file.path() << std::endl;
+					return 1;
+				}
+			}
+		}		
+  } else {
+		auto path = std::string(argv[1]);
+		auto success = convert(path);
+		if (!success) {
+			std::cerr << "Failed to convert " << path << std::endl;
+			return 1;
+		}
+	}
 }
