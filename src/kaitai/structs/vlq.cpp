1364b4ecc8014577408a07b36bfd8a22171238c8
Abheek Dhawan <abheekd@protonmail.com>
2024-01-17 13:41:10 -0600

    fix: update includes for new source files

--- src/kaitai/structs/vlq.cpp
+++ src/kaitai/structs/vlq.cpp
@@ -1,6 +1,6 @@
 // This is a generated file! Please edit source .ksy file and use kaitai-struct-compiler to rebuild
 
-#include "vlq.h"
+#include "kaitai/structs/vlq.h"
 
 vlq_t::vlq_t(kaitai::kstream* p__io, kaitai::kstruct* p__parent, vlq_t* p__root) : kaitai::kstruct(p__io) {
     m__parent = p__parent;
